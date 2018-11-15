95081351684b5aff0aecf8b612830ffd608bd19f
Wonho ha <haho013039@gmail.com>
2018-11-15 11:54:11 +0900

    INIT Project

---
+++ Express/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: '45.76.222.210',
+    user: 'root',
+    password: '6644',
+    port: '6644',
+    database: 'Lila',
+    insecureAuth: true
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+});
+
+module.exports = router;
